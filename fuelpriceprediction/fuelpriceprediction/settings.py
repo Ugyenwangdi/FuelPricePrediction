5773ba408a4d4fd5b1106753ab7ad7262b61c98f
Ugyenwangdi <57824653+Ugyenwangdi@users.noreply.github.com>
2022-06-29 00:39:17 +0600

    Update settings.py

--- fuelpriceprediction/fuelpriceprediction/settings.py
+++ fuelpriceprediction/fuelpriceprediction/settings.py
@@ -25,7 +25,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ux94zl0r+8kca_+x9hmja)zvt-0&r!u%sw1&)_nq3cs9#en=&k'
+SECRET_KEY = 'YOUR SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
