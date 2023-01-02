43a31333ad8d4a68846990dfc12eb8fd9a533db8
Ugyenwangdi <12190105.gcit@rub.edu.bt>
2023-01-02 23:38:51 +0600

    update

--- fuelpriceprediction/fuelpriceprediction/settings.py
+++ fuelpriceprediction/fuelpriceprediction/settings.py
@@ -25,11 +25,11 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'YOUR SECRET KEY'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['fuelpriceprediction.herokuapp.com', 'localhost', '127.0.0.1'] 
+ALLOWED_HOSTS = ['fuelpriceprediction.herokuapp.com', 'localhost', '127.0.0.1', 'fuelpriceprediction.onrender.com'] 
 
 # Application definition
 
