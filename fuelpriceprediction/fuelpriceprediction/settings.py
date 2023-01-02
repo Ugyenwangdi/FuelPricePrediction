7242c0b62a8d12a02406ad2ea7502bb5dc17b96b
Ugyenwangdi <12190105.gcit@rub.edu.bt>
2023-01-03 00:21:44 +0600

    update

--- fuelpriceprediction/fuelpriceprediction/settings.py
+++ fuelpriceprediction/fuelpriceprediction/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
+    'fuelpriceprediction.core',
     'django_plotly_dash.apps.DjangoPlotlyDashConfig',
     'channels',
 ]
