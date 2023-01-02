51aa73fccef9212035e4c87a921f05a23930fc0b
Ugyenwangdi <12190105.gcit@rub.edu.bt>
2023-01-03 00:09:59 +0600

    update

--- fuelpriceprediction/fuelpriceprediction/wsgi.py
+++ fuelpriceprediction/fuelpriceprediction/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fuelpriceprediction.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fuelpriceprediction.fuelpriceprediction.settings')
 
 application = get_wsgi_application()
